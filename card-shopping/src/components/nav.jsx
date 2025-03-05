e721b8a8c6a9f88b07243f7f5ab2f2f3526ed41f
parastoopourazizi <napourazizi@gmail.com>
2025-03-05 20:18:52 +0330

    edit menu and add basket icon

--- card-shopping/src/components/nav.jsx
+++ card-shopping/src/components/nav.jsx
@@ -1,19 +1,25 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
+
 const Nav = () => {
   return (
-    <div className="navbar  navbar-expand-lg">
+    <div className="navbar navbar-dark bg-dark navbar-expand-lg">
       <div className="container">
-        <a className="navbar-brand"> cooding</a>
+        <a className="navbar-brand">cooding</a>
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link to="/">shop</Link>
           </li>
           <li className="nav-item">
-            <Link to="/card">card</Link>
+            <Link to="/card">
+              <FontAwesomeIcon icon={faShoppingBasket} />
+            </Link>
           </li>
         </ul>
       </div>
     </div>
   );
 };
+
 export default Nav;
