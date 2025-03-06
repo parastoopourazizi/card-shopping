60821fbe208bd6d0f07d48b9d87aea1f8ad9f01f
parastoopourazizi <napourazizi@gmail.com>
2025-03-06 20:50:50 +0330

    add data product

---
+++ card-shopping/src/data/prohucts.js
@@ -0,0 +1,37 @@
+import mobile from "../assets/mobile.jpg";
+import laptop from "../assets/laptop.jpg";
+import camera from "../assets/camera.jpg";
+import denim from "../assets/denim.jpg";
+import shoes from "../assets/shoes.jpg";
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Mobile",
+    price: 1000,
+    productItem: mobile,
+  },
+  {
+    id: 2,
+    productName: "Laptop",
+    price: 2000,
+    productItem: laptop,
+  },
+  {
+    id: 3,
+    productName: "Camera",
+    price: 3000,
+    productItem: camera,
+  },
+  {
+    id: 4,
+    productName: "Denim",
+    price: 15000,
+    productItem: denim,
+  },
+  {
+    id: 5,
+    productName: "Shoes",
+    price: 2000,
+    productItem: shoes,
+  },
+];
