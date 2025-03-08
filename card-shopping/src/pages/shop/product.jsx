a1299c895e3eb06363d16c3904a0a268df993afc
parastoopourazizi <napourazizi@gmail.com>
2025-03-08 19:42:30 +0330

    add other props

--- card-shopping/src/pages/shop/product.jsx
+++ card-shopping/src/pages/shop/product.jsx
@@ -1,6 +1,11 @@
 const Product = (props) => {
-  return <div className="col-3">
-    {props.data.productName}
-  </div>
+  const { id, productName, productImage, price } = props.data;
+  return (
+    <div className="col-3">
+      <img src={productImage} className="w-100" />
+      <h5>{productName}</h5>
+      <p>price : {price}</p>
+    </div>
+  );
 };
 export default Product;
