8794ef1675529fabe980083be6610d62902a3f2b
parastoopourazizi <napourazizi@gmail.com>
2025-03-25 01:48:34 +0330

    add shop context

--- card-shopping/src/context/shopContext.jsx
+++ card-shopping/src/context/shopContext.jsx
@@ -6,12 +6,22 @@ export const shopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState([]);
 
   const addToCart = (itemId) => {
-    setCartItems([...cartItems, { id: itemId, count: 1 }]);
+    if (!cartItems?.find((item) => item.id === itemId))
+      setCartItems([...cartItems, { id: itemId, count: 1 }]);
+    else
+      setCartItems(
+        cartItems.map((item) => {
+          if (item.id === itemId) return { ...item, count: item.count + 1 };
+          else return item;
+        })
+      );
   };
   const removeFromCart = (itemId) => {
     setCartItems(
       cartItems.map((i) => {
-        if (i.id === itemId) return { ...i, count: i.count - 1 };
+        if (i.id === itemId)
+          return { ...i, count: i.count === 0 ? 0 : i.count - 1 };
+        else return i;
       })
     );
   };
--- card-shopping/src/pages/shop/product.jsx
+++ card-shopping/src/pages/shop/product.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { shopContext } from "../../context/shopContext";
 
 const Product = (props) => {
   const { id, productName, productImage, price } = props.data;
-  const [cartItems ,setCartItems ]= useState([])
+  const { cartItems, addToCart, removeFromCart } = useContext(shopContext);
   return (
     <div className="col-3">
       <img src={productImage} className="w-100" />
       <h5>{productName}</h5>
       <p>price : {price}$ </p>
-      <button className="btn btn-info btn-sm">+</button>
-      <span className="mx-1">0</span>
-      <button className="btn btn-info btn-sm">-</button>
+      <button className="btn btn-info btn-sm" onClick={() => addToCart(id)}>
+        +
+      </button>
+      <span className="mx-1">
+        {cartItems.find((item) => item.id === id)?.count || 0}
+      </span>
+
+      <button
+        className="btn btn-info btn-sm"
+        onClick={() => removeFromCart(id)}
+      >
+        -
+      </button>
     </div>
   );
 };
